61cdef39dacd06b02c55f77473ce6096ec4680b9
Nitinxtech <nitinp3006@gmail.com>
2025-02-16 00:50:34 +0530

    db operations

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
+
+
 const {InfoController} = require('../../controllers');
+const airplaneRoutes = require('./airplane-routes');
+
+router.use('/airplanes', airplaneRoutes)
+
 
 router.get("/info", InfoController.info);
+
 
 module.exports = router;
