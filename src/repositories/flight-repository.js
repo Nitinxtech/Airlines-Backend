237081345a5bd1a701ea37a793914cf1833a3b98
Nitinxtech <nitinp3006@gmail.com>
2025-03-02 17:50:46 +0530

    Flight service complete setup

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,7 +1,9 @@
 const {Sequelize} = require('sequelize');
 
 const CrudRepository = require('./crud-repository');
-const {Flight, Airplane, Airport, City} = require('../models')
+const {Flight, Airplane, Airport, City, sequelize} = require('../models');
+const db = require('../models');
+const {addRowLockOnFlights} = require('./queries');
 
 
 class FlightRepository extends CrudRepository {
@@ -46,6 +48,24 @@ class FlightRepository extends CrudRepository {
             ]
         });
         return response;
+    }
+
+    async updateRemainingSeats(flightId, seats, dec = true) { 
+        const transaction = await db.sequelize.transaction();
+        try {
+            await db.sequelize.query(addRowLockOnFlights(flightId));
+            const flight = await Flight.findByPk(flightId);
+            if(+dec) {
+                await flight.decrement('totalSeats', {by: seats}, {transaction: transaction})
+            } else {
+                await flight.increment('totalSeats', {by: seats}, {transaction: transaction});
+            }
+            await transaction.commit();
+            return flight;
+        } catch (error) {
+            await transaction.rollback();
+            throw error;
+        }
     }
 }
 
--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -19,4 +19,9 @@ router
     .get('/:id',
         FlightController.getFlight);
 
+router
+    .patch('/:id/seats',
+        FlightMiddlewares.validateUpdateSeatsRequest,
+        FlightController.updateSeats);
+
 module.exports = router;
